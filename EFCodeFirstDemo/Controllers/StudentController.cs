0d48a5e3ac162596f9597158278c74ff8621ce04
PAPPU KUMAR KESHARI <92083274+Kesharipappu@users.noreply.github.com>
2022-03-12 21:43:05 -0800

    Add project files.

---
+++ EFCodeFirstDemo/Controllers/StudentController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCodeFirstDemo.Models;
+using EFCodeFirstDemo.Migrations;
+using System.Data.Entity;
+
+
+namespace EFCodeFirstDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        PGDACContext db=new PGDACContext();//creating DBContext object for db operations
+        // GET: Student
+        public ActionResult Index()
+        {
+            //select * from table
+            //return View(db.Students.ToList());
+            return View(db.Students.Include("Address").ToList());
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        //insert Code to save student data
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<PGDACContext,Configuration>());
+
+          //  Database.SetInitializer(new DropCreateDatabaseAlways<PGDACContext>()); //Don't use this command otherwisw all data will be deleted
+            if(ModelState.IsValid)
+            {
+                db.Students.Add(student);//Adding current entity in context
+                db.SaveChanges();//saving in data base
+               // ViewBag.Message = "Student Data Saved Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(student);
+            
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EFCodeFirstDemo/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCodeFirstDemo.Models
+{
+    //[Table(name:"Student_Master")]
+    public class Student
+    {
+        public int StudentID { get; set; } //scaler property
+        //[Required()]
+        //[Display(Name ="Student Name")]
+        public string StudentName { get; set; }    //scaler property
+
+        //[Required]
+        public string DateOfBirth { get; set; }//scaler property
+         public string Email { get; set; }
+        public Address Address { get; set; } //Navigation property
+    }
+    public class Address
+    {
+        public int Id { get; set; }
+        //[Required]
+        public string AddressLine { get; set; }
+       // [Required(ErrorMessage ="City Name is Mandatory")]
+        public string City { get; set; }
+        public ICollection<Student> Students { get; set; }
+
+    }
+
+}
